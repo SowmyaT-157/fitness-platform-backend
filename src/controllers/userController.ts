c20459c1a60dc244297d6bc5678a022d8a3df330
sowmya <sowmya.t@everest.engineering>
2026-03-01 18:36:16 +0530

    Update the controllers for the create user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,9 +2,13 @@ import { Request,Response } from "express"
 import { newPerson } from "../services/userServices"
 
 export const createNewPerson = async (req:Request, res:Response) => {
+    console.log("enter into controller")
     try {
+        console.log("enter try block")
         const data = req.body
+        console.log(data,"request data going")
         const newTask = await newPerson(data)
+        console.log(newTask, "data comming..")
         if (newTask) {
             return res.status(201).json({ message: "successfully created", newTask })
         } else {
