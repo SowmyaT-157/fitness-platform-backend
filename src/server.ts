7429e4b9b86d7be9198e1bbb0ce3cc807330912c
sowmya <sowmya.t@everest.engineering>
2026-03-01 18:37:35 +0530

    Update the server for add the endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import sequelize from './config/dbConnection';
+import { router } from './routes/userRoutes';
 
 
 const app = express();
 app.use(express.json());
 
+
 const PORT = process.env.PORT
+app.use("/", router)
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
