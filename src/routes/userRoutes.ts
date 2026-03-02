3e8ef19a137a453220ff9549b514d6358fac90f1
sowmya <sowmya.t@everest.engineering>
2026-03-02 16:51:14 +0530

    Update the routes to add signin and signup

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { registerUser, signInUser } from "../controllers/userController";
 import { createNewPerson } from "../controllers/userController";
 
 export const router = Router();
 router.post('/user',createNewPerson)
+router.post('/signUp',registerUser )
+router.post('/signIn',signInUser)
