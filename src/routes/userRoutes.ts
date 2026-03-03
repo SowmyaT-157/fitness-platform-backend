ba4fa5b68e8d9ba0ede1cec69a2da5bdf09270cd
sowmya <sowmya.t@everest.engineering>
2026-03-03 16:16:39 +0530

    Update the router to remove the router

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import { registerUser, signInUser } from "../controllers/userController";
-import { createNewPerson } from "../controllers/userController";
 
 export const router = Router();
-router.post('/user',createNewPerson)
 router.post('/signUp',registerUser )
 router.post('/signIn',signInUser)
