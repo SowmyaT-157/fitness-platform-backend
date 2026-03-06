9db4a1afe1aee16a4c5e8855a282759dff7cb130
sowmya <sowmya.t@everest.engineering>
2026-03-06 11:58:40 +0530

    Updaet the useRouter to add the verify code

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/userController";
+import { registerUser, verifyEmail } from "../controllers/userController";
 
 export const router = Router();
 router.post('/signUp',registerUser )
+router.get('/verify', verifyEmail)
+
 // router.post('/signIn',signInUser)
