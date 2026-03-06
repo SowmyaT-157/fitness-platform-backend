6749dfa108084f0b9b8154ae4fd6d41d29d96bf3
sowmya <sowmya.t@everest.engineering>
2026-03-06 10:49:49 +0530

    Update the dbconnection to change the db credentials

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('fitness', 'sowmya1', '1234', {
+const sequelize = new Sequelize('fitnes', 'sowmya1', '1234', {
   host: 'localhost',
   dialect: 'postgres' 
 });
