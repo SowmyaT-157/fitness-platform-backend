1cc4e88c50ab2ab1b72f03940cd85987ff5117ab
sowmya <sowmya.t@everest.engineering>
2026-03-07 00:39:29 +0530

    Update the server to add the cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import sequelize from './config/dbConnection';
 import { router } from './routes/userRoutes';
-
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 const PORT = process.env.PORT
 app.use("/", router)
