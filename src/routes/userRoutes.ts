3ce7696b26a38bf7d97658f926b1f394449a79d7
sowmya <sowmya.t@everest.engineering>
2026-03-07 21:30:54 +0530

    Update the routers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { registerUser, signInUser, verifyEmail } from "../controllers/userController";
+import { presigned, registerUser, signInUser, verifyEmail } from "../controllers/userController";
 
 export const router = Router();
 router.post('/signUp', registerUser)
 router.post('/verify', verifyEmail)
 router.post('/signIn',signInUser)
+router.put('/image',presigned)
