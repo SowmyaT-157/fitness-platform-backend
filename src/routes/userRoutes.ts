504613ec514e19b4e36da4085291f5c4175c130a
sowmya <sowmya.t@everest.engineering>
2026-03-10 00:43:48 +0530

    Update the routes to add patch method

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { presigned, registerUser, signInUser, verifyEmail } from "../controllers/userController";
+import { presigned, registerUser, signInUser, updateImage, verifyEmail } from "../controllers/userController";
 
 export const router = Router();
 router.post('/signUp', registerUser)
 router.post('/verify', verifyEmail)
 router.post('/signIn',signInUser)
 router.put('/image',presigned)
+router.patch('/newImage',updateImage)
